f8ea8a4680be34ae35cb08c2f6d303eb1c7a9366
Đặng Xuân Minh Hiếu <zipdang2004@gmail.com>
2024-01-20 02:34:21 +0700

    change unit test

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,25 +1,12 @@
-namespace TTHV.UnitTest;
+namespace UnitTest;
 
-[TestClass]
-public class UnitTest1 : PageTest
+public class Tests
 {
-    [TestMethod]
-    public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage() {
-        await Page.GotoAsync("https://playwright.dev");
+    [SetUp]
+    public void Setup() { }
 
-        // Expect a title "to contain" a substring.
-        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
-
-        // create a locator
-        var getStarted = Page.Locator("text=Get Started");
-
-        // Expect an attribute "to be strictly equal" to the value.
-        await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
-
-        // Click the get started link.
-        await getStarted.ClickAsync();
-
-        // Expects the URL to contain intro.
-        await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
+    [Test]
+    public void Test1() {
+        Assert.Pass();
     }
 }
